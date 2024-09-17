83960198b0fe32eb68f20c9b1b08dc2277ceabd9
Dina Ahmed <denaahmed340@gmail.com>
2024-09-18 01:06:26 +0300

    track error

--- script.js
+++ script.js
@@ -49,9 +49,11 @@ form.onsubmit = (e)=>{
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "send_email.php", true);
+    
 
     xhr.onload = ()=>{
         if(xhr.readyState == 4 && xhr.status == 200){
+            print("inside status 200");
         let response = xhr.response;
         if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
             statusTxt.style.color = "red";
